429ced0279e18ee5a67bb0bd78f6712f5af98bd8
Rafael Pimenta <jrafapimenta@gmail.com>
2022-09-15 16:46:05 +0100

    adicionar disp

--- locker/devices/routes.py
+++ locker/devices/routes.py
@@ -30,4 +30,16 @@ def landing():
 def conectar_dispositivo():
     title = "Adicionar Novo Dispositivo"
     form = NovoDispositivoForm()
+    if form.validate_on_submit():
+        nome_disp = form.nome.data
+        pin_rasp = form.pin.data
+        device = Devices.query.filter_by(pin = pin_rasp).first()
+        if device:
+            flash(f"Pino a ser utilizado por {device.nome}", "danger")
+            return redirect(url_for('devices.conectar_dispositivo'))
+        device_add = Devices(nome = nome_disp, pin = pin_rasp, estado = False)
+        db.session.add(device_add)
+        db.session.commit()
+        flash(f"Dispositivo {nome_disp} no pino {pin_rasp} adicionado com sucesso", "success")
+        return redirect(url_for("devices.landing"))
     return render_template("devices/adicionar_device.html", title = title, form = form)
