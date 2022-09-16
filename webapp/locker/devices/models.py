95b4e350cf5ccd2b824ef99dca9c056a619d1892
Rafael Pimenta <jrafapimenta@gmail.com>
2022-09-16 23:24:45 +0100

    melhor devisao de pastas servidor/scrpits_rasp

---
+++ webapp/locker/devices/models.py
@@ -0,0 +1,22 @@
+from locker import db
+
+"""
+    Modelo para armazenar as configurações dos Relés
+        id - ID de device
+        nome - Nome do device
+        pin - Pin number ao qual device esta ligado
+        estado - Ligado ou Desligado
+"""
+
+class Devices(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(50),unique = True, nullable = False)
+    pin = db.Column(db.Integer, unique = True, nullable = False)
+    estado = db.Column(db.Boolean, nullable = False)
+
+class Raspberry(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(50), nullable = False, unique = True)
+    key = db.Column(db.String(50), nullable = False, unique = False)
+
+db.create_all()
